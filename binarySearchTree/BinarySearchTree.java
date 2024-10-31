19e4b92252ca312f5276096312ebfbaa507f9e58
humanbeing60 <hellocdac0060@gmail.com>
2024-10-31 13:36:31 +0530

    managed some file code conflicts in bST package

--- binarySearchTree/BinarySearchTree.java
+++ binarySearchTree/BinarySearchTree.java
@@ -1,7 +1,5 @@
 package com.cdac.binarySearchTree;
 
-import com.cdac.treeTraversal.Traversal;
-
 public class BinarySearchTree implements BST {
 	
 	private BSTNode root;
@@ -30,72 +28,65 @@ public class BinarySearchTree implements BST {
 		root =  insertNode(root, value); 
 	}
 	
-	private BSTNode deleteNode(BSTNode root, int value) {
-	    if (root == null) {
-	        return null; 
-	    }
-
-	    if (value == root.value) {
-	    	
-	        if (root.left != null && root.right != null) {
-	          // Both children exist
-	        	/* 
-		        	//if using predecessor
-		            BSTNode predecessor = Helper.getPredecessor(root.left);
-		            root.value = predecessor.value;                       // Replace value with predecessor's value
-		            root.left = deleteNode(root.left, predecessor.value); // Delete the predecessor
-	        	 */
-	        
-	            //if using Successor
-	            BSTNode successor = Helper.getSuccessor(root.right);
-	            root.value = successor.value; // Replace value with successor's value
-	            root.right = deleteNode(root.right, successor.value); // Delete the successor
-	            
-	            
-	        } else if (root.left == null) {
-	        	// only right childNode exists
-	            return root.right; 
-	        } else {
-	            // Only left childNode exists
-	            return root.left; 
-	        }
-	    } else if (value > root.value) {
-	        root.right = deleteNode(root.right, value);
-	    } else {
-	        root.left = deleteNode(root.left, value);
-	    }
-	    return root;
+	// finding the successor
+	private BSTNode getSuccessor(BSTNode succ) {
+		while(succ.left != null) {
+			succ = succ.left;
+		}
+		return succ;
 	}
-// ===== my code issue =========================================================================
-//			if(root.left != null && root.right != null) {
-//			// both child exists
-//				//BSTNode succPred = Helper.getSuccessor(root.right);
-//				BSTNode succPred = Helper.getPredecessor(root.left);
-//
-//				// swapping current nodes value with its succPred value
-//				int temp = root.value;
-//				root.value = succPred.value;
-//				succPred.value = temp;
-//				
-//				//return deleteNode(succPred,value);
-//				//succPred = null;
-//				return root;
-//			}
-// ==============================================================================
+	
+//	= DELETE NODE IN BST =============================================================================
+	
+	private BSTNode deleteNode(BSTNode root, int value) {
+		if(root == null) {
+			return null;
+		}
+		
+		if(value == root.value) {
+			if(root.left != null && root.right != null) {
+			// both child exists
+				// inorder Successor
+				BSTNode successor = getSuccessor(root.right);
+
+				// swaping current nodes value with its successor value
+				int temp = root.value;
+				root.value = successor.value;
+				successor.value = temp;
+				
+				successor = null;
+				return root;
+			}else if(root.left == null && root.right != null) {
+			// only right node exists
+				return root.right;
+			}else if(root.left != null && root.right == null) {
+			// only left node exists
+				return root.left;
+			}else {
+			// leaf node
+				return null;
+			}
+			
+		} else if(value > root.value) {
+			root.right = deleteNode(root.right,value);
+		} else {
+			root.left = deleteNode(root.left,value);
+		}
+		
+		return root;
+	}
 	@Override
 	public void delete(int value) {
 		root = deleteNode(root, value);
 	}
 	
+//	======================================================================================
 	
 	@Override
 	public void traverse() {
 		Traversal.levelorder(root);
 		System.out.println("");
 		System.out.println("========== END =========");
-		Traversal.inOrder(root);
-		System.out.println("");
-		System.out.println("========== end1  =========");
 	}
 
 	
@@ -141,5 +132,5 @@ public class BinarySearchTree implements BST {
 	public int findLargest() {
 		return largestElement(root);
 	}
-	
+
 }
