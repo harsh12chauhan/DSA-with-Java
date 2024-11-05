a14c3b7a5bc49db6e3752582edc8e9d01bf96c15
humanbeing60 <hellocdac0060@gmail.com>
2024-11-05 23:34:20 +0530

    completed stack

--- stack/interfaces/MyStack.java
+++ stack/interfaces/MyStack.java
@@ -1,10 +1,9 @@
 package com.cdac.stack.interfaces;
 
 public interface MyStack {
-	public void push();
-	public int pop();
+	public void push(int data);
+	public void pop();	
 	public int top();
 	public boolean isEmpty();
 	public boolean isFull();
-	public void printStack();
 }
--- stack/stackMain/StackMain.java
+++ stack/stackMain/StackMain.java
@@ -1,9 +1,23 @@
 package com.cdac.stack.stackMain;
 
+import com.cdac.stack.impl.MyStackClass;
+import com.cdac.stack.interfaces.MyStack;
+
 public class StackMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Stack");
+		MyStack st = new MyStackClass();
+		
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		
+		while(!st.isEmpty()) {			
+			System.out.println(st.top());
+			st.pop();
+		}
+		
+		System.out.println(st.top());
 	}
-
 }
